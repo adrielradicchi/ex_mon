561c427bc8874aa22f56c02e2267dac5e3b41b4f
Adriel Radicchi <adriel.nfsc@gmail.com>
2020-08-21 22:49:55 -0300

    begin write tests

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -9,6 +9,13 @@ defmodule ExMon.Game.Actions do
     end
   end
 
+  def heal() do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
+    end
+  end
+
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
@@ -19,16 +26,6 @@ defmodule ExMon.Game.Actions do
     Enum.find_value(moves, {:error}, fn {key, value} ->
       if value == move, do: {:ok, key}
     end)
-    #   Enum.find_value(moves, {:error}, &search_move/2 )
-  end
-
-  # def search_move({key, value}, move) when value == move do: {:ok,}
-
-  def heal() do
-    case Game.turn() do
-      :player -> Heal.heal_life(:player)
-      :computer -> Heal.heal_life(:computer)
-    end
   end
 
 end
